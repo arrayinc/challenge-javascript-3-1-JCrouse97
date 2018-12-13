13c01bab9a1447fe5104a3ddecd4b8b1307765ca
JCrouse97 <jcrouse@thearrayschool.com>
2018-12-13 11:48:42 -0700

    all the beer!

--- answer.js
+++ answer.js
@@ -1,1 +1,14 @@
 // *** YOUR ANSWER BELOW ***
+
+function bottlesOfBeer(x){
+  let versus = ""
+  for(let i = x; i > 0; i--){
+    versus += `${i} of beer on the wall! ${i} bottle of beer, take one down, pass it around! `;
+
+  }
+
+  return versus;
+
+}
+
+bottlesOfBeer(10);
